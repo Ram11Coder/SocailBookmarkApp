669b05b69c76800e5a65b629fb6a5b54ee933c3f
Ram11Coder <rambeliver11@gmail.com>
2021-12-23 00:09:40 +0530

    Added constant export classes

---
+++ src/com/thrillio/constants/UserType.java
@@ -0,0 +1,14 @@
+package com.thrillio.constants;
+
+//Constant export class
+public class UserType {
+
+	private UserType() {
+
+	}
+
+	public static final String USER = "user";
+	public static final String EDITOR = "editor";
+	public static final String CHIEF_EDITOR = "cheifeditor";
+
+}
